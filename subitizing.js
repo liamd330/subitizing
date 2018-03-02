70e5923a058dbd866f19e15edd06baa025e47630
Liam Donovan <liamdonovan245@gmail.com>
2018-03-02 11:58:19 -0500

    removed monochrome option

--- subitizing.js
+++ subitizing.js
@@ -14,9 +14,12 @@ var numberOfShapes = Math.ceil(Math.random() * numberOfShapesInput);
 do {
     var xCoord = Math.floor(Math.random() * 350);
     var yCoord = Math.floor(Math.random() * 350);
+
     var red = Math.floor(Math.random() * 256);
     var blue = Math.floor(Math.random() * 256);
     var green = Math.floor(Math.random() * 256);
+    
+
     ctx.fillStyle = 'rgb(' + red + ',' + blue + ',' + green + ')';
     ctx.fillRect(xCoord, yCoord, 50, 50);
     numberOfShapes -= 1;
@@ -24,8 +27,6 @@ do {
 
 while (numberOfShapes > 0);
     
-
-
 var timer = document.getElementById("millisecondInput").value;
 
 function clear() {
