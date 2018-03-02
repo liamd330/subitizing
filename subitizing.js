89b010ec9477d75a37ee11b3edfc1fcf12897ba0
Liam Donovan <liamdonovan245@gmail.com>
2018-03-02 11:34:35 -0500

    timer working

--- subitizing.js
+++ subitizing.js
@@ -1,10 +1,16 @@
+
+
+function go() {
+
 var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+var ctx = canvas.getContext('2d');    
+    
+ctx.clearRect(0, 0, 400, 400);
+    
+var numberOfShapesInput = document.getElementById("numberInput").value;
+var numberOfShapes = Math.ceil(Math.random() * numberOfShapesInput);
 
-
-// var numberOfShapes = document.getElementById("numberInput");
-var numberOfShapes = Math.ceil(Math.random() * 10);
-
+    
 do {
     var xCoord = Math.floor(Math.random() * 350);
     var yCoord = Math.floor(Math.random() * 350);
@@ -18,3 +24,14 @@ do {
 
 while (numberOfShapes > 0);
     
+
+
+var timer = document.getElementById("millisecondInput").value;
+
+function clear() {
+    ctx.clearRect(0, 0, 400, 400);
+}
+
+setTimeout(clear, timer);
+    
+    }
